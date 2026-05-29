e4078c1e7f09731f26fae03c14faa457bbac2681
Rafaell3 <21520240032@maristabrasil.g12.br>
2026-05-29 10:24:49 -0300

    Implementação do JS e atualização do CSS

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // --- EFEITO DIGITAÇÃO ---
+    const textElement = document.querySelector(".container h3");
+    if (textElement) {
+        const fullText = textElement.textContent;
+        textElement.textContent = ""; 
+        
+        let index = 0;
+        function typeWriter() {
+            if (index < fullText.length) {
+                textElement.textContent += fullText.charAt(index);
+                index++;
+                setTimeout(typeWriter, 45);
+            }
+        }
+        setTimeout(typeWriter, 800);
+    }
+
+    // --- EFEITO PISCAR--
+    const title = document.querySelector(".container h1");
+    if (title) {
+        function flickerEffect() {
+            title.style.opacity = Math.random() > 0.08 ? "1" : "0.4";
+            
+            let randomTime = Math.random() * 500 + 50; 
+            setTimeout(flickerEffect, randomTime);
+        }
+        flickerEffect();
+    }
+});
